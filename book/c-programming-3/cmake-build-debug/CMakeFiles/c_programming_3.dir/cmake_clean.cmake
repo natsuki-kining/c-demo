aff827c84ae672f7060d280b2f166956d02ed078
natsuki-kining <natsuki_kining@126.com>
2020-02-03 17:15:15 +0800

    hello world

---
+++ book/c-programming-3/cmake-build-debug/CMakeFiles/c_programming_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_programming_3.dir/main.c.obj"
+  "c_programming_3.exe"
+  "c_programming_3.exe.manifest"
+  "c_programming_3.pdb"
+  "libc_programming_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_programming_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
