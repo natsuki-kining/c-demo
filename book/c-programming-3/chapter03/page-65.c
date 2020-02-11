d2091f68949dd4ab086a42553a046a943ff6a367
natsuki-kining <natsuki_kining@126.com>
2020-02-11 19:55:08 +0800

    逗号表达式

---
+++ book/c-programming-3/chapter03/page-65.c
@@ -0,0 +1,15 @@
+//
+// Created by Inori on 2020/2/11.
+//
+
+//逗号表达式
+
+#include <stdio.h>
+void main(){
+    int a = 2,b = 2,c = 2;
+    a = (3,a*3);
+    b = 3 * 5 , b * 6;//不生效
+    c = (3,(c * 3, (c * 3, c * 4 )));
+
+    printf("a:%d,b:%d,c:%d",a,b,c);
+}
