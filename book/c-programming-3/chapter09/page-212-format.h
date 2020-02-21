df774d31966b9edcbc2b8f32dc5b52f037766bd5
natsuki-kining <natsuki_kining@126.com>
2020-02-21 22:03:49 +0800

    include

---
+++ book/c-programming-3/chapter09/page-212-format.h
@@ -0,0 +1,12 @@
+//
+// Created by Inori on 2020/2/21.
+//
+
+#define PR printf
+#define NL "\n"
+#define D "%d"
+#define D1 D NL
+#define D2 D D NL
+#define D3 D D D NL
+#define D4 D D D D NL
+#define S "%s"
---
+++ book/c-programming-3/chapter09/page-212-macro-definition-include.c
@@ -0,0 +1,15 @@
+//
+// Created by Inori on 2020/2/21.
+//
+#include <stdio.h>
+#include "page-212-format.h"
+
+void main(){
+    int a=1,b=2,c=3,d=4;
+    char string[] = "CHINA";
+    PR(D1,a);
+    PR(D2,a,b);
+    PR(D3,a,b,c);
+    PR(D4,a,b,c,d);
+    PR(S,string);
+}
